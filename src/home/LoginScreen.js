9f03ea2ac870e46a09f17385cc3a28eba204fe4d
louis.y <louis.y.2.tcg@gmail.com>
2024-06-04 15:36:08 +0800

    feat: login event trigger and logic

--- src/home/LoginScreen.js
+++ src/home/LoginScreen.js
@@ -7,9 +7,19 @@ import {
   Switch,
   Image,
   ScrollView,
+  Alert,
 } from "react-native";
+import React, { useState } from "react";
 
 export default function LoginScreen() {
+  let userObject = {
+    account: "",
+    password: "",
+    rememberMe: false,
+  };
+
+  const [loginInfo, setLoginInfo] = useState(userObject);
+
   return (
     <ScrollView className="flex-1" style={styles.container}>
       <View className="items-center justify-top">
@@ -24,27 +34,52 @@ export default function LoginScreen() {
         </Text>
         <View className="my-3 w-10/12">
           <TextInput
+            value={loginInfo.account}
+            onChangeText={(text) =>
+              setLoginInfo({ ...loginInfo, account: text })
+            }
             placeholder={"email"}
             className="px-3 py-2 border border-blue-300 rounded-md bg-blue-50"
           />
         </View>
         <View className="my-3 w-10/12">
           <TextInput
+            value={loginInfo.password}
+            onChangeText={(text) =>
+              setLoginInfo({ ...loginInfo, password: text })
+            }
             placeholder={"password"}
             secureTextEntry={true}
             className="px-3 py-2 border border-blue-300 rounded-md bg-blue-50"
           />
         </View>
         <View className="flex-row my-5">
-          <Switch value={true} />
+          <Switch
+            value={loginInfo.rememberMe}
+            onValueChange={(value) => {
+              setLoginInfo({ ...loginInfo, rememberMe: value });
+            }}
+          />
           <Text className="pt-3 pl-5 text-white">remember me</Text>
         </View>
-        <TouchableOpacity className="bg-indigo-700 rounded-lg my-3">
+        <TouchableOpacity
+          className="bg-indigo-700 rounded-lg my-3"
+          onPress={onLogin}
+        >
           <Text className="text-white text-xl px-5 py-3">Login</Text>
         </TouchableOpacity>
       </View>
     </ScrollView>
   );
+
+  function onLogin() {
+    if (loginInfo.account === "" || loginInfo.password === "") {
+      Alert.alert("Please enter email and password");
+      return false;
+    } else {
+      Alert.alert("Login success");
+    }
+  }
 }
 
 const styles = StyleSheet.create({
